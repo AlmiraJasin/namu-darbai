b0ad7c95889c17b75fc81b97c0c0a7c2fa1e77e5
Almira Jasinskyte <jasinskyte.almira@gmail.com>
2022-04-07 20:14:58 +0300

    funkcijos 6-10 uzd

--- april-1-functions-nd.js
+++ april-1-functions-nd.js
@@ -1,5 +1,5 @@
-/* 1. Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.
-*/
+// 1. Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.
+
 console.log('1. -----------');
 
 function dviejuSuma(num1, num2) {
@@ -25,8 +25,8 @@ function skaiciuPalyginimas(num1, num2) {
 
 console.log(skaiciuPalyginimas(12, 12));
 
-/* 3. Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.
-*/
+// 3. Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.
+
 
 console.log('3. -----------');
 
@@ -40,8 +40,8 @@ function keliamujuMetuPatikra(duotiejiMetai) {
 
 console.log(keliamujuMetuPatikra(1700));
 
-/*4. Parašyti funkciją, kuri priimtų vieną skaičių. Grąžinti skaičiaus kvadratą.
-*/
+// 4. Parašyti funkciją, kuri priimtų vieną skaičių. Grąžinti skaičiaus kvadratą.
+
 
 console.log('4. -----------');
 
@@ -51,13 +51,16 @@ function skaiciausKvadratas(num) {
 
 console.log(skaiciausKvadratas(12));
 
-/*5. Parašyti funkciją, kuri priimtų vieną skaičių, kuris turi būti didesnis už vienetą. 
+/* 5. Parašyti funkciją, kuri priimtų vieną skaičių, kuris turi būti didesnis už vienetą. 
 Funkcija turi grąžinti visų skaičių sumą, nuo 1 iki įvesto skaičiaus.
 */
 
 console.log('5. -----------');
 
 function skaiciuSuma(num) {
+    if (num <= 1) {
+        return 'Negalima reiksme';
+    }
     let suma = 0;
     for (let i = 1; i <= num; i++) {
         suma += i;
@@ -67,33 +70,90 @@ function skaiciuSuma(num) {
 
 console.log(skaiciuSuma(6));
 
-/*6. Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos 
+/* 6. Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos 
 (išskyrus vienetą ir patį save).
 */
 
 console.log('6. -----------');
 
+function dalikliuSkaicius(num) {
+    let dalmenys = 0
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            dalmenys += 1;
+        }
+    }
+    return dalmenys;
+} 
 
+console.log(dalikliuSkaicius(12));
 
-/*7. Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą. 
+/* 7. Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą. 
 Funkcija turi išvesti tekstą į ekraną ir dar papildomai parodyti jo ilgį(simbolių kiekį).
 */
 
 console.log('7. -----------');
 
-/*8. Parašyti funkcijątelefonoNumeris, kuri priima vieną kintamąjį–masyvą su 10 skaičių jame.
+function tekstas(text) {
+    let galune = 'simboliu';
+    if(text.length === 1) {
+        galune = 'simboli';
+    }
+    return `Tekstas "${text}" turi ${text.length} ${galune}`;  
+}
+
+console.log(tekstas('a'));
+console.log(tekstas('akjgvnd'));
+
+/* 8. Parašyti funkciją telefonoNumeris, kuri priima vieną kintamąjį–masyvą su 10 skaičių jame.
 Funkcija turi grąžinti telefono numerį tokiu formatu -"(XXX) XXX-XXXX".
 */
 
 console.log('8. -----------');
 
-/*9. Parašyti funkciją, kuri priimtų du kintamuosius-tekstus.Palyginti kuris tekstas yra ilgesnis.
-*/
+const vartotojoNumeris = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+function telefonoNumeris(Skaiciai) {
+    let formatas = '(XXX) XXX-XXXX';
+    for (let i = 0; i <= vartotojoNumeris.length; i++) {
+        formatas = formatas.replace('X', Skaiciai[i])
+    }
+    return formatas;
+}
+
+console.log(telefonoNumeris(vartotojoNumeris));
+
+// 9. Parašyti funkciją, kuri priimtų du kintamuosius-tekstus.Palyginti kuris tekstas yra ilgesnis.
 
 console.log('9. -----------');
 
-/*10. Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą.Suskaičiuoti, kiek tekste yra ‚a‘ raidžių.
-*/
+function tekstoIlgioPalyginimas(text1, text2) {
+    if (text1.length > text2.length) {
+        return `${text1} ilgesnis uz ${text2}`;
+    } else if (text1.length < text2.length) {
+        return `${text1} trumpesnis uz ${text2}`;
+    } else {
+        return `${text1} ir ${text2} vienodo ilgio`;
+    }
+}
+
+console.log(tekstoIlgioPalyginimas('Java', 'JavaScript'));
+console.log(tekstoIlgioPalyginimas('JavaScrip', 'Java'));
+console.log(tekstoIlgioPalyginimas('Java', 'Java'));
+
+
+// 10. Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą.Suskaičiuoti, kiek tekste yra ‚a‘ raidžių.
 
 console.log('10. -----------');
 
+function aRaidesTekste(text) {
+    let aRaides = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === 'a') {
+            aRaides += 1;
+        }
+    }
+    return aRaides;
+} 
+
+console.log(aRaidesTekste('JavaScript'));
